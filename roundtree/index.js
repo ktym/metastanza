ba9253940ac7ff279c4e47db4ff2c371af603838
Toshiaki Katayama <k@bioruby.org>
2018-04-07 01:20:15 +0900

    Initial import

---
+++ roundtree/index.js
@@ -0,0 +1,21 @@
+Stanza(function(stanza, params) {
+  console.log(params)
+  stanza.render({
+    template: "stanza.html",
+    parameters: {
+      title: params.title
+    }
+  })
+  function draw(json) {
+    var config = { selector: stanza.select("#result") }
+    for (key in params) {
+      if (params[key]) {
+        config[key] = params[key]
+      }
+    }
+    console.log(config)
+    d3sparql.roundtree(json, config)
+    console.log(json)
+  }
+  d3sparql.fetch(params.sparql_api, draw)
+});
